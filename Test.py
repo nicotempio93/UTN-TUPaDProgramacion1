69f0d2adf3b6ff35db2a512ea9b7bec8a74e121d
Nicolas Tempio <nicotempio93@gmail.com>
2025-09-06 20:46:05 +0200

    Agregamos tp4

--- Test.py
+++ Test.py
@@ -1,19 +1,8 @@
-dia = int(input("Ingrese el día (1-31): "))
-mes = int(input("Ingrese el mes (1-12): "))
-hemisferio = input("Ingrese el hemisferio (N/S): ").upper()
+num = int(input("Ingrese un nuemero: "))
 
-mesInvierno = [ 1, 2]
-mesPrimavera = [ 4, 5]
-mesVerano = [7, 8]
-mesOtono = [10, 11]
-estacion = ""
+largo = len(str(num))
+invertido = ""
 
-if (mes == 1 or mes == 2) or (mes == 12 and dia >= 21 or mes == 3 and dia <= 20):
-    estacion = "Invierno" if hemisferio == "N" else "Verano"
-elif (mes == 4 or mes == 5) or (mes == 3 and dia >= 21 or mes == 6 and dia <= 20):
-    estacion = "Primavera" if hemisferio == "N" else "Otoño"
-elif (mes == 7 or mes == 7) or (mes == 6 and dia >= 21 or mes == 9 and dia <= 20):
-        estacion = "Verano" if hemisferio == "N" else "Invierno"
-elif (mes == 10 or mes == 11) or (mes == 9 and dia >= 21 or mes == 12 and dia <= 20):
-        estacion = "Otoño" if hemisferio == "N" else "Primavera"
-print("La estacion es: ", estacion)
+for i in range(largo):
+    invertido += str(num)[largo -1 - i]
+print(invertido)
